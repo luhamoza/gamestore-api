15f92e8410481a92467aee1d94358fd0e2d37a3a
luhamoza <luqmanhafizimohdzawawi@gmail.com>
2024-03-17 06:46:26 +0800

    feat: add map group

--- Program.cs
+++ Program.cs
@@ -27,10 +27,10 @@ List<Game> games = new List<Game>()
 const string getGameEndPointName = "Get Game";
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var group = app.MapGroup("/games");
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/games", () => games);
-app.MapGet("/games/{id}", (int id) =>
+group.MapGet("/", () => games);
+group.MapGet("/{id}", (int id) =>
 {
    Game? game = games.Find(game => game.Id == id);
    if (game == null)
@@ -39,13 +39,13 @@ app.MapGet("/games/{id}", (int id) =>
    }
    return Results.Ok(game);
 }).WithName(getGameEndPointName);
-app.MapPost("/games", (Game game) =>
+app.MapPost("/", (Game game) =>
 {
     game.Id = games.Max(game => game.Id) + 1;
     games.Add(game);
     return Results.CreatedAtRoute(getGameEndPointName, new { id = game.Id }, game);
 });
-app.MapPut("/games/{id}", (Game updateGame, int id) =>
+app.MapPut("/{id}", (Game updateGame, int id) =>
 {
     Game? existingGame = games.Find(game => game.Id == id);
     if (existingGame is null)
@@ -59,7 +59,7 @@ app.MapPut("/games/{id}", (Game updateGame, int id) =>
     existingGame.ImageUri = updateGame.ImageUri;
     return Results.NoContent();
 });
-app.MapDelete("/games/{id}", (int id) =>
+app.MapDelete("/{id}", (int id) =>
 {
     Game? game = games.Find(game => game.Id == id);
     if (game is not null)
@@ -67,7 +67,6 @@ app.MapDelete("/games/{id}", (int id) =>
         games.Remove(game);
         return Results.NotFound();
     }
-
     return Results.NoContent();
 });
 
