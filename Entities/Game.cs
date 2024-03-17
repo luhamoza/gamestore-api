8702ab1f1c8cd91f24b0100579f989353c76eda8
luhamoza <luqmanhafizimohdzawawi@gmail.com>
2024-03-17 11:12:09 +0800

    feat: add data annotations for game entity

--- Entities/Game.cs
+++ Entities/Game.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.api.Entities;
 
 public class Game
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(50)]
     public required string Name { get; set; }
+    [Required]
+    [StringLength(20)]
     public required string Genre { get; set; }
+    [Required]
+    [Length(0,1000)] 
     public decimal Price { get; set; }
     public DateTime ReleaseDate { get; set; }
+    [Url]
+    [StringLength(100)]
     public required string ImageUri { get; set; }
 }
